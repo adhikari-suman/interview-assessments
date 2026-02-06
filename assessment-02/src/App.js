6e7acae81fece928b71e3b9f86ef8433d8d27eae
Suman Adhikari <expertxkid@gmail.com>
2026-02-05 19:11:34 -0600

    init(assessment-02): init assessment

--- assessment-02/src/App.js
+++ assessment-02/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    setCount(count + 5);
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Initial count bug</h1>
+      {/* Implement the buttons once bug is fixed. */}
+      <button>Increment by 5</button>
+      &nbsp;
+      <button> Decrement by 5</button>
+      {/* BUG: Counter keeps on running. Please fix it. */}
+      <p>Count: {count}</p>
     </div>
   );
 }
