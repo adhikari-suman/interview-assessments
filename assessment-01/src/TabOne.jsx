7fe403488220806d571fdf2b0edc3b17b9967d18
Suman Adhikari <expertxkid@gmail.com>
2026-02-05 18:56:21 -0600

    feat: add TabOne and TabTwo

---
+++ assessment-01/src/TabOne.jsx
@@ -0,0 +1,10 @@
+function TabOne() {
+  return (
+    <div>
+      <h2>Tab 1</h2>
+      <p>This is the content for Tab 1.</p>
+    </div>
+  );
+}
+
+export default TabOne;
---
+++ assessment-01/src/TabTwo.jsx
@@ -0,0 +1,10 @@
+function TabTwo() {
+  return (
+    <div>
+      <h2>Tab 2</h2>
+      <p>This is the content for Tab 2.</p>
+    </div>
+  );
+}
+
+export default TabTwo;
